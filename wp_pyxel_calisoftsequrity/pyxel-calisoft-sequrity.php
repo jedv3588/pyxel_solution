c352878aea40384c20fbbcf5b6e6a40b7abdffe4
Julio Enrique Duque Vizcaino <jduque@cigb.edu.cu>
2020-12-08 15:09:10 -0500

    adding not reuse password function

--- wp_pyxel_calisoftsequrity/pyxel-calisoft-sequrity.php
+++ wp_pyxel_calisoftsequrity/pyxel-calisoft-sequrity.php
@@ -10,6 +10,9 @@ Author URI: https://github.com/jedv3588
 License: GPL2
 */
 defined('ABSPATH') or die('Access denied');
+include( plugin_dir_path( __FILE__ ) . 'includes/ppr-class.php');
+
+register_activation_hook( __FILE__, 'ppr_install' );
 
 /*
  * Variables globales
@@ -77,6 +80,9 @@ function wp_pyxel_script_close_inactive_sesion()
 }
 add_action('admin_head', 'wp_pyxel_script_close_inactive_sesion');
 
+/*
+ * close inactive session
+ */
 function expiration_session_id( $expiration, $user_id, $remember ) {
     return $remember ? $expiration : 600;
     $sessions = WP_Session_Tokens::get_instance($user_id);
@@ -86,6 +92,9 @@ function expiration_session_id( $expiration, $user_id, $remember ) {
 }
 add_filter( 'auth_cookie_expiration', 'expiration_session_id', 99, 3 );
 
+/*
+ * destroy closed session id
+ */
 function destroy_session_id( $user_id ) {
     if ( ! isset ( $_GET['logout'] ) ){
         $sessions = WP_Session_Tokens::get_instance($user_id);
@@ -119,11 +128,14 @@ function pyxel_force_strong_passwords( $errors, $update, $user_data ) {
         if ( ! preg_match( '/[A-Z]/', $user_pass ) ) {
             $errors->add( 'my_pass_uppercase', __( 'La contraseña debe tener al menos una mayuscula', 'your_textdomain' ) );
         }
+        if( !preg_match('/[!@#$%^&*()]/', $user_pass) ) {
+            $errors->add( 'my_pass_simbol', __( 'La contraseña debe tener al menos un carácter especial [! @ # $ % ^ & * ( )]', 'your_textdomain' ) );
+        }
     }
 }
 add_action( 'user_profile_update_errors', 'pyxel_force_strong_passwords', 0, 3 );
 
-// Restrict back-end for administrators only
+/* Restrict back-end for administrators only
 function restrict_admin_area_by_rol() {
     if ( ! current_user_can( 'manage_options' ) && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
         wp_redirect( site_url() );
@@ -191,6 +203,24 @@ add_action( 'login_enqueue_scripts', function ()
     }
     wp_add_inline_script( 'jquery-migrate', 'jQuery(document).ready(function(){ jQuery( "#pass1" ).data( "reveal", 0 ); });' );
 }, 1 );
+
+//Personalized logo
+function my_login_logo() {
+    global $url_plugin_pyxel;
+    ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo $url_plugin_pyxel . '/imagenes/logo1.png'; ?>);
+            height:108px;
+            width:320px;
+            background-size: 320px 108px;
+            background-repeat: no-repeat;
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
 /*add_filter( 'wpum_get_registration_fields', 'wpum_my_psw_field_confirmation' );
 
 function wpum_verify_my_psw_confirmation( $pass, $fields, $values, $form ) {
@@ -213,3 +243,83 @@ add_filter( 'submit_wpum_form_validate_fields', 'wpum_verify_my_psw_confirmation
 
 //register_activation_hook(__FILE__.array($jedvPlugin, 'activate'));
 //register_deactivation_hook(__FILE__.array($jedvPlugin, 'deactivate'));
+*/
+
+/*
+ * Functions to prevent password reuse
+ */
+//Lets Install the plugin
+function ppr_install()
+{
+    global $wpdb;
+    global $ppr_db_version;
+    $table_name = $wpdb->prefix . "password_log";
+
+    $sql = "CREATE TABLE $table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+    user mediumint(9) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    UNIQUE KEY id (id)
+    );";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    add_option( "ppr_db_version", $ppr_db_version );
+}
+
+
+//Lets check the password doesn't match the current password or any of our stored passwords on password reset
+add_action( 'validate_password_reset', 'check_password_history', 10, 2 );
+function check_password_history( $errors, $user )
+{
+
+    if ( !empty($_POST['pass1']) && $_POST['pass1'] == $_POST['pass2'] ) {
+
+        $ppr = new ppr($user);
+
+        //Check against current password
+        if (! $ppr->check_current_password($_POST['pass1'])) {
+            $errors->add( 'duplicate_password', __( '<strong>ERROR</strong>: Esta contraseña ha sido usada anteriormente por usted, debe seleccionar una contraseña unica diferente a las anteriores' ) );
+        }
+
+        //Check against all previous passwords
+        if (! $ppr->check_previous_passwords($_POST['pass1'])) {
+            $errors->add( 'duplicate_password', __( '<strong>ERROR</strong>: Esta contraseña ha sido usada anteriormente por usted, debe seleccionar una contraseña unica diferente a las anteriores' ) );
+        }
+    }
+}
+
+//Lets check the password doesn't match the current password or any of our stored passwords on profile update
+add_filter('user_profile_update_errors', 'check_fields', 10, 3);
+function check_fields($errors, $update, $user) {
+
+    if ($update && isset($_POST['pass1']) && !empty($_POST['pass1']) && $_POST['pass1'] == $_POST['pass2']) {
+
+        $ppr = new ppr($user);
+
+        //Check against current password
+        if (! $ppr->check_current_password($_POST['pass1'])) {
+            $errors->add( 'duplicate_password', __( '<strong>ERROR</strong>: Esta contraseña ha sido usada anteriormente por usted, debe seleccionar una contraseña unica diferente a las anteriores' ) );
+        }
+
+        //Check against all previous passwords
+        if (! $ppr->check_previous_passwords($_POST['pass1'])) {
+            $errors->add( 'duplicate_password', __( '<strong>ERROR</strong>: Esta contraseña ha sido usada anteriormente por usted, debe seleccionar una contraseña unica diferente a las anteriores' ) );
+        }
+
+        if ( empty($errors->errors) ) {
+            $ppr->store_old_password();
+        }
+    }
+}
+
+//Lets store the users old password each time a new one is saved
+add_action( 'password_reset', 'store_hashed_password', 10, 2 );
+function store_hashed_password( $user, $new_pass )
+{
+    $ppr = new ppr($user);
+    $ppr->store_old_password();
+}
+
